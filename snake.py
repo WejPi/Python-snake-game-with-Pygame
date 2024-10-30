e9ffa41a73f1a6e87a72b8905534c3646134f3ac
Piotr <piotrwejda@gmail.com>
2024-10-30 14:46:50 +0100

    first commit

---
+++ snake.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Snake():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def moveHead(self):
+        pass
+    def moveBody(self):
+        pass
+    def addBody(self):
+        pass
+
+class Body():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+
+class Food():
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+
+
+def main():
+    pass
+
+
+
+
+
+main()
